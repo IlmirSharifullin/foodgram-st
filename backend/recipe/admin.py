86478d4a5e8c94f378d745a7939eab6091c9f378
IlmirSharifullin <ilmirsharifullin2000@gmail.com>
2025-06-26 21:08:10 +0300

    set admin configs for models

--- backend/recipe/admin.py
+++ backend/recipe/admin.py
@@ -1,3 +1,37 @@
 from django.contrib import admin
+from .models import Ingredient, Recipe, RecipeIngredient
 
-# Register your models here.
+
+class IngredientAdmin(admin.ModelAdmin):
+    list_display = (
+        "name",
+        "measurement_unit",
+    )
+    list_display_links = ("name",)
+    list_editable = ("measurement_unit",)
+    search_fields = ("name",)
+
+
+class RecipeAdmin(admin.ModelAdmin):
+    search_fields = (
+        "name",
+        "author__email",
+    )
+    list_display = (
+        "name",
+        "author",
+    )
+    list_display_links = ("name",)
+    list_editable = ("author",)
+
+    readonly_fields = ("get_favorite_count",)
+
+    def get_favorite_count(self, obj):
+        return obj.in_favorites.count()
+
+    get_favorite_count.short_description = "Добавление(-й) в избранное"
+
+
+admin.site.register(Ingredient, IngredientAdmin)
+admin.site.register(Recipe, RecipeAdmin)
+admin.site.register(RecipeIngredient)
--- backend/user/admin.py
+++ backend/user/admin.py
@@ -1,3 +1,55 @@
 from django.contrib import admin
+from .models import User, Follow, Favorite, ShoppingCart
 
-# Register your models here.
+
+class UserAdmin(admin.ModelAdmin):
+    search_fields = ("email", "username")
+    list_display = (
+        "email",
+        "username",
+        "first_name",
+        "last_name",
+        "avatar",
+        "is_subscribed",
+    )
+    list_editable = (
+        "username",
+        "first_name",
+        "last_name",
+        "avatar",
+        "is_subscribed",
+    )
+    list_display_links = ("email",)
+
+
+class FollowAdmin(admin.ModelAdmin):
+    list_display = (
+        "user",
+        "following",
+    )
+    list_display_links = ("user",)
+    list_editable = ("following",)
+
+
+class ShoppingCartAdmin(admin.ModelAdmin):
+    list_display = (
+        "user",
+        "recipe",
+    )
+    list_display_links = ("user",)
+    list_editable = ("recipe",)
+
+
+class FavoriteAdmin(admin.ModelAdmin):
+    list_display = (
+        "user",
+        "recipe",
+    )
+    list_display_links = ("user",)
+    list_editable = ("recipe",)
+
+
+admin.site.register(User, UserAdmin)
+admin.site.register(Follow, FollowAdmin)
+admin.site.register(ShoppingCart, ShoppingCartAdmin)
+admin.site.register(Favorite, FavoriteAdmin)
